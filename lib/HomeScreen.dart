c32421800b07a92ecfacd68e653096955e3bbba5
FarhanAlmasyhur <51041665+FarhanAlmasyhur@users.noreply.github.com>
2020-11-14 13:24:32 +0800

    Finishing HomeScreen

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -48,15 +48,182 @@ class HomeScreen extends StatelessWidget {
                     border: InputBorder.none),
               ),
             ),
+
             Container(
-              margin: EdgeInsets.symmetric(vertical: 25),
+              margin: EdgeInsets.only(top: 25),
               height: 200,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  
+                  SizedBox(
+                    width: 5,
+                  ),
+
+                  //Baris Pertama
+
+                  //inhaler pertama
+                  Container(
+                    margin: EdgeInsets.only(top: 2, right: 16, bottom: 2),
+                    width: 160,
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 245, 174, 1),
+                        borderRadius: BorderRadius.circular(2),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 4,
+                            offset: Offset(0, 2),
+                          )
+                        ]),
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          child: Container(
+                            height: 150,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image:
+                                    AssetImage('assets/images/respiclick.png'),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          child: Container(
+                            width: 160,
+                            height: 48,
+                            color: Colors.white,
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Nama Inhaler',
+                              style: TextStyle(
+                                  fontSize: 20, fontFamily: "Raleway"),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+
+                  //Inhaler Kedua
+                  Container(
+                    margin: EdgeInsets.only(top: 2, right: 16, bottom: 2),
+                    width: 160,
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 245, 174, 1),
+                        borderRadius: BorderRadius.circular(2),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 4,
+                            offset: Offset(0, 2),
+                          )
+                        ]),
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          child: Container(
+                            height: 150,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image:
+                                    AssetImage('assets/images/respiclick.png'),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          child: Container(
+                            width: 160,
+                            height: 48,
+                            color: Colors.white,
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Nama Inhaler',
+                              style: TextStyle(
+                                  fontSize: 20, fontFamily: "Raleway"),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+
+                  //Inhaler Ketiga
+                  Container(
+                    margin: EdgeInsets.only(top: 2, right: 16, bottom: 2),
+                    width: 160,
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 245, 174, 1),
+                        borderRadius: BorderRadius.circular(2),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 4,
+                            offset: Offset(0, 2),
+                          )
+                        ]),
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          child: Container(
+                            height: 150,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image:
+                                    AssetImage('assets/images/respiclick.png'),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          child: Container(
+                            width: 160,
+                            height: 48,
+                            color: Colors.white,
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Nama Inhaler',
+                              style: TextStyle(
+                                  fontSize: 20, fontFamily: "Raleway"),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    
+
+                  ),
+                ],
+              ),
+
+            ),
+            //END OF INHALER ATAS
+
+
+            //Inhaler Bawah
+
+            Container(
+              height: 200,
+              margin: EdgeInsets.only(top: 20),
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: <Widget>[
                   SizedBox(
                     width: 5,
                   ),
+
+                  //inhaler pertama
                   Container(
                     margin: EdgeInsets.only(top: 2, right: 16, bottom: 2),
                     width: 160,
@@ -71,30 +238,40 @@ class HomeScreen extends StatelessWidget {
                             offset: Offset(0, 2),
                           )
                         ]),
-                    alignment: Alignment.bottomCenter,
                     child: Stack(
                       children: <Widget>[
-                        Container(
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('assets/images/respiclick.png'),
+                        Positioned(
+                          child: Container(
+                            height: 150,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image:
+                                    AssetImage('assets/images/respiclick.png'),
+                              ),
                             ),
                           ),
                         ),
-                        Container(
-                          width: 160,
-                          height: 48,
-                          color: Colors.white,
-                          alignment: Alignment.center,
-                          child: Text(
-                            'Nama Inhaler',
-                            style:
-                                TextStyle(fontSize: 20, fontFamily: "Raleway"),
+                        Positioned(
+                          bottom: 0,
+                          child: Container(
+                            width: 160,
+                            height: 48,
+                            color: Colors.white,
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Nama Inhaler',
+                              style: TextStyle(
+                                  fontSize: 20, fontFamily: "Raleway"),
+                            ),
                           ),
                         )
                       ],
                     ),
                   ),
+                  //END OF INHALER PERTAMA
+
+                  //inhaler Kedua
                   Container(
                     margin: EdgeInsets.only(top: 2, right: 16, bottom: 2),
                     width: 160,
@@ -109,7 +286,40 @@ class HomeScreen extends StatelessWidget {
                             offset: Offset(0, 2),
                           )
                         ]),
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          child: Container(
+                            height: 150,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image:
+                                    AssetImage('assets/images/respiclick.png'),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          child: Container(
+                            width: 160,
+                            height: 48,
+                            color: Colors.white,
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Nama Inhaler',
+                              style: TextStyle(
+                                  fontSize: 20, fontFamily: "Raleway"),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
+                  //END OF INHALER KEDUA
+
+                  //Inhaler Ketiga
                   Container(
                     margin: EdgeInsets.only(top: 2, right: 16, bottom: 2),
                     width: 160,
@@ -124,9 +334,43 @@ class HomeScreen extends StatelessWidget {
                             offset: Offset(0, 2),
                           )
                         ]),
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          child: Container(
+                            height: 150,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image:
+                                    AssetImage('assets/images/respiclick.png'),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          child: Container(
+                            width: 160,
+                            height: 48,
+                            color: Colors.white,
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Nama Inhaler',
+                              style: TextStyle(
+                                  fontSize: 20, fontFamily: "Raleway"),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
+                  //END OF INHALER KETIGA
+
                 ],
               ),
+              //END OF LISTVIEW
+
             )
           ],
         ),
